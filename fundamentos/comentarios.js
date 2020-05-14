e198aba649b8c5cccb0c446ed7dbbd2ac9ad7999
ophelipecamilo <pfernandescamilo@gmail.com>
2020-05-13 23:10:36 -0300

    Initial commit

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,15 @@
+// Comentário de uma linha
+console.log('linha 1')
+
+/*
+Comentário de 
+multiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+*   Comentário
+*   de múltiplas
+*   linhas
+*/
+console.log('Linha 3')
---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // Infinito
+console.log('10'/2)
+console.log('Show!'* 2)
+console.log(0.1 + 0.7)
+// console.log(10.toString()) Não é possível
+console.log((10.345).toFixed(2))
