b52d8f3b80d19cbe73cbd34c058fd939494ab382
ophelipecamilo <pfernandescamilo@gmail.com>
2020-06-01 20:34:53 -0300

    organize

---
+++ fundamentos/aritimeticos.js
@@ -0,0 +1,11 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo) // binário, usando mais de uma variável
+
+console.log(soma, subtracao, multiplicacao, -divisao, modulo) // Unário, transformou a divisão em negativo
---
+++ fundamentos/operadoreslogicos.js
@@ -0,0 +1,33 @@
+/* 
+v e v -> v
+v e f -> f
+f e ? -> f
+
+v ou ? -> v
+f ou v -> v
+f ou f -> f
+
+v xor v -> f
+v xor f -> v
+f xor v -> v
+f xor f -> f
+
+xor = exclusivo ter que ser iguais
+
+!v -> f
+!f -> v
+*/
+
+function compras(trabalho1, trabalho2) {
+    const comprarSorvete = trabalho1 || trabalho2 // operador ou
+    const comprarTv50 = trabalho1 && trabalho2 // operador E
+    // const comprarTv32 = !!(trabalho1 ^ trabalho2) // bitwise xor Operador bit a bit
+    const comprarTv32 = trabalho1 != trabalho2
+    const manterSaudavel = !comprarSorvete // operador unário
+
+    return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel }
+}
+console.log(compras(true, true))
+console.log(compras(true, false))
+console.log(compras(false, true))
+console.log(compras(false, false))
