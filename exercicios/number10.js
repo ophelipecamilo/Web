002663bcb27926d5cf69d43dcdcdc2816dbcb7d4
ophelipecamilo <pfernandescamilo@gmail.com>
2020-06-22 06:48:18 -0300

    new2

---
+++ exercicios/number10.js
@@ -0,0 +1,11 @@
+function Divisivel3 (numero) {
+    if(numero % 3 == 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(Divisivel3(3))
+console.log(Divisivel3(2))
+console.log(Divisivel3(150))
---
+++ exercicios/number8.js
@@ -0,0 +1,22 @@
+let Stpontuacoes = "10 20 20 8 25 3 0 30"
+
+function AvaliarPontos (Stpontuacoes) {
+    let pontuacoes = Stpontuacoes.split(", ")
+    let qtdQuebraDeRecords = 0
+    let piorJogo = 1
+    let maiorPontuacao = pontuacoes[0]
+    let menorPontuacao = pontuacoes[0]
+
+    for (let i = 1; i < pontuacoes.length; i++) {
+        if(pontuacoes[i] > maiorPontuacao) {
+            maiorPontuacao = pontuacoes[i]
+            qtdQuebraDeRecords++
+        }else if (pontuacoes[i] < menorPontuacao) {
+            menorPontuacao = pontuacoes[i]
+            piorJogo = i+1;
+        }
+    }
+    return [qtdQuebraDeRecords, piorJogo]
+}
+ 
+console.log(AvaliarPontos(Stpontuacoes))
